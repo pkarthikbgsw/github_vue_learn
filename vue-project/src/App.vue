5e99cfa4e523beb904ad5a009de92122c988a845
kpl4cob <karthik.palanisamy@in.bosch.com>
2026-03-30 07:39:20 +0100

    Implementation of the function

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,21 +1,33 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+/* Variable declaration */
 const count = ref(0)
+
+/* Function declaration */
+function increment() {
+  count.value++
+}
+
 </script>
 
 <template>
+
+  <!-- Header -->
   <h1>You did it!</h1>
+
+  <!-- Paragraph -->
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
 
+  <!-- Rendering logic -->
   <div id="app">
-  <button @click="count++">
+  <button @click="increment">
     Count is: {{ count }}
   </button>
-</div>
+  </div>
 
 </template>
 
